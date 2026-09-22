995a458c2eab1b6c099ffa3b8053a28ddc8a3815
shilohmtc <shilohmtc@gmail.com>
2026-09-22 22:42:05 +0200

    Keep My Shiloh Home Storybook action singular

--- stories/MyShilohPwa.stories.js
+++ stories/MyShilohPwa.stories.js
@@ -194,6 +194,7 @@ export const AuthenticatedHomeSummaryActions = {
       });
       const action = document.createElement('a');
       action.className = 'button button--primary experience-primary';
+      action.dataset.clientExperiencePrimary = '';
       action.href = '/book';
       action.textContent = 'Book an appointment';
       home.appendChild(action);
